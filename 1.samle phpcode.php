e328c3d41918949cb209abcde402d18690dcaff8
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-01 01:46:14 +0600

    first commit

--- 1.samle phpcode.php
+++ 1.samle phpcode.php
@@ -1,3 +1,9 @@
 <?php
-    echo "php practice now start ";
+    //single comment line
+    /*multiple
+    comment line */
+
+    echo "php practice now start " . "<br>";  // dot means concatenate 
+    echo "php programming". "<br>" .  10+10;
+    phpinfo();   //all of php format rules show page 
 ?>
