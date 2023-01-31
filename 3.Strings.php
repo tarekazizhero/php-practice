ac8ba0ee6068c9ef1681653528cdf13fad5b0d9b
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-01 03:29:03 +0600

    String to a variable

---
+++ 3.Strings.php
@@ -0,0 +1,28 @@
+<?php
+    $var1="hello anis-";
+    $var2="what do you do?-";
+    $var3="your address give me- ";
+    $var3=$var1 ."how are you and-" . $var2 ;
+    echo $var3. "<br>";
+
+    $var1="hello anis-";
+    $var2="what do you do?-";
+    //$var3="your address give me- ";
+    $var3.=$var1 ."how are you and-" . $var2 ;
+    /*
+    variable er pore jodi dot add kore deya hy ($var3.) and same variabvle jodi na thake ,tobe oi variable er under a value gulo 2 bar print korbe. */
+    echo $var3 . "<br>";
+
+
+    $var1="hello anis-";
+    $var2="what do you do?-";
+    $var3="your address give me- ";
+    $var3.=$var1 ."how are you and-" . $var2 ;
+    /* 
+    jodi ($var3) purbe declare thake ,tahole seti print korar sathe $var3 er curret value gulo print korbe 
+    */
+    echo $var3;
+
+    
+
+?>
