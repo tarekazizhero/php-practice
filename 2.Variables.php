98a7b1e36d5a8fa3f74cdaf7d33edabc02bfa74c
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-01 02:18:48 +0600

    value assign to variable

---
+++ 2.Variables.php
@@ -0,0 +1,17 @@
+<?php
+    /*bariables rules
+    1/dollar sign at the beginnig
+    2/case sensitive
+    3/no space allowed
+    4/letters,numbers,and dashes are not allowed */
+    $name="aziz"; //$name is a variable and "aziz" is a value
+    echo $name . "<br>";  //print variable
+
+
+    $Book=45; //$book,$Book are a two different variable
+    $book=78;
+    echo $book . "<br>";
+    echo $Book;
+
+
+?>
