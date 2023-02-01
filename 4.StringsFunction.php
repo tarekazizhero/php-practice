901ad2bee9dc487177e8eaa0ed70b3cb0abb34de
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-01 11:13:06 +0600

    String Function

---
+++ 4.StringsFunction.php
@@ -0,0 +1,14 @@
+<?php
+    $myString ="this is a texT book";
+    echo strtoupper($myString) . "<br>"; //all are the capital letter
+    echo strtolower($myString) . "<br>"; //all are letter be smalled
+    echo ucwords($myString) . "<br>"; //proty ti word er first letter capital hba 
+    echo ucfirst($myString) . "<br>"; //sentece er 1st word er 1st letter capital hbe 
+    echo trim($myString, "ok") . "<br>"; //remove null,"\n",defined charecter
+
+    echo "strlen:" . strlen($myString) . "<br>"; //ktogula string ace
+    echo "strstr:" . strstr($myString, "a") . "<br>"; //
+    echo "strReplace:" . str_replace("is","are",$myString);
+
+
+?>
