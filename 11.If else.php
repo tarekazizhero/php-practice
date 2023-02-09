a872a9553f39fb702f8265f9dab6867a77ea65da
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-10 01:07:40 +0600

    if else statement

---
+++ 11.If else.php
@@ -0,0 +1,17 @@
+<?php
+    $x=0;
+    $y=23;
+
+    if($x>$y){
+ echo "x boro";
+    }
+    elseif($x>0){
+        echo"x boro";
+    }
+    elseif($x<0){
+        echo "x coto";
+    }
+    else{
+        echo "y boro";
+    }
+?>
