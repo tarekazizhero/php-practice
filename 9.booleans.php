91c0c51c3f2ca51dc2eb1d45cbdf0d080991de32
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-09 15:32:11 +0600

    booleans add

---
+++ 9.booleans.php
@@ -0,0 +1,18 @@
+booleans
+<br><br>
+<?php
+ $num= true;
+ $num1=false;
+  echo "True=" .$num . "<br>";
+  echo "False=" .$num1;
+?>
+
+<br><br>
+cheack 
+<br><br>
+<?php
+ $ed= 1;
+ $ed1=0.1;
+ var_dump($ed) . "<br>";
+ var_dump($ed1) . "<br>";
+?>
