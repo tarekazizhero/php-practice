51833899cb9db484b83864e70b21795f6a3a6ab7
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-11 00:37:52 +0600

    logical operators

---
+++ 13.logicalOperators.php
@@ -0,0 +1,28 @@
+ <?php
+    $x=20;
+    $y=40;
+
+    if($x==20 and $y==40){
+        echo "hellow word";  //and a 2 ti value true hte hba  tahole output show korbe 
+    }
+   echo "<br>";
+   if($x==20 && $y==40){
+    echo "hellow word";  //and a 2 ti value true hte hba  tahole output show korbe 
+}
+echo "<br>";
+    if($x==20 or $y==45)
+    {
+        echo "hellow world"; //or a jekono akti mile gelei show korbe 
+    }
+    echo "<br>";
+    if($x==20 || $y==45)
+    {
+        echo "hellow world"; //or a jekono akti mile gelei show korbe 
+    }
+
+    echo "<br>";
+ if($x !==23){
+    echo "hello world";
+ }
+
+ ?>
