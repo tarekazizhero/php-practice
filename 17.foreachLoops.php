197fccce6166ceb2c17ab3f3949bfa8abf4071e3
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-11 23:52:37 +0600

    foreach loops

---
+++ 17.foreachLoops.php
@@ -0,0 +1,13 @@
+<?php
+    $name =array('aziz','tarek','sohan');
+    foreach($name as $value){
+        echo $value . '<br>';
+    }
+?>
+<br><br>
+<?php
+   $age=array("aziz"=>"34","tarek"=>"35","sohan"=>"54");
+   foreach($age as $x=>$value){
+    echo "$x=$value <br>";
+   }
+?>
