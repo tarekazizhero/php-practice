d2188cdbcd65a2285df64347452c76de4e45522b
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-11 15:55:08 +0600

    for loop

---
+++ 15.whileLoops.php
@@ -0,0 +1,17 @@
+<?php
+    $x=1;
+    while($x<=5){
+        echo "the number serial is : $x <br> ";
+        $x++;
+        
+    }
+?>
+<br><br><br>
+<?php
+    $x=0;
+    while($x<=200){
+        echo "the number serial is : $x <br> ";
+        
+        $x+=10;
+    }
+?>
---
+++ 16.forLoops.php
@@ -0,0 +1,18 @@
+<?php
+    for($x=0;$x<=8; $x++){
+        echo"the serial number of : $x <br>";
+
+    }
+
+    // $x=0 initialize the loop counter start  0  
+// $x<=8 continue the loop as long as $x is less then or equal to 8
+//$x++ increase the loop counter value by 1 for each iteration
+?>
+
+<br><br><br>
+<?php
+    for($x=0;$x<=20; $x+=5){
+        echo"the serial number of : $x <br>";
+
+    }
+?>
