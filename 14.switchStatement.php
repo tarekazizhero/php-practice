a324604fbf176cac8a0dd9f8240d99c4d3f8f2d5
tarekazizhero <1720114.tarekaziz@gmail.com>
2023-02-11 14:34:59 +0600

    switch case

---
+++ 14.switchStatement.php
@@ -0,0 +1,24 @@
+<?php
+   /* switch(value){
+        case
+        code;
+        case
+        code;
+    } */
+$num="red";
+$num1="blue";
+    switch($num){
+        case $num1:
+        echo "here is a red color";
+        break;
+
+        case $num1:
+        echo "here is a blue color";
+        break;
+            default :
+            echo "no have not a value";
+            break;
+    
+    }
+
+?>
